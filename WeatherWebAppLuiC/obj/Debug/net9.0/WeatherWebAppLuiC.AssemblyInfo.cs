9ac737fcfc828a7f14a5d94b2ddf48f815cc9194
LContreras678 <luicacoustic@gmail.com>
2025-10-17 21:57:12 +0100

    git commit -m

--- WeatherWebAppLuiC/obj/Debug/net9.0/WeatherWebAppLuiC.AssemblyInfo.cs
+++ WeatherWebAppLuiC/obj/Debug/net9.0/WeatherWebAppLuiC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherWebAppLuiC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f2c9b2634a586ad0a0d5990c7f3d1a2f635efa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820746457699e8630cfea09bb1dd5328659e793c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherWebAppLuiC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherWebAppLuiC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WeatherWebAppLuiC/obj/Release/net9.0/WeatherWebAppLuiC.AssemblyInfo.cs
+++ WeatherWebAppLuiC/obj/Release/net9.0/WeatherWebAppLuiC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherWebAppLuiC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+243fdc222d83b880b2b8ac50f813110905374653")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820746457699e8630cfea09bb1dd5328659e793c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherWebAppLuiC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherWebAppLuiC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
