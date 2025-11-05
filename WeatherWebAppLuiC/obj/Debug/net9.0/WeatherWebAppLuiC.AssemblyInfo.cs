4f5aa2ad3521f846b4060fe2b0357c404470638f
LContreras345 <luimarcontreras09@gmail.com>
2025-11-05 21:47:35 +0000

    git commit -m

--- WeatherWebAppLuiC/obj/Debug/net9.0/WeatherWebAppLuiC.AssemblyInfo.cs
+++ WeatherWebAppLuiC/obj/Debug/net9.0/WeatherWebAppLuiC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherWebAppLuiC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820746457699e8630cfea09bb1dd5328659e793c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac737fcfc828a7f14a5d94b2ddf48f815cc9194")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherWebAppLuiC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherWebAppLuiC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
